b0ad19e6b145b4554724eff424c34232f3e3318b
R-scan1 <deepr7590@gmail.com>
2025-03-07 12:29:54 +0530

    [Raj] feat. validated the name inputted by user

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';  
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-root',
-  standalone: true,  
-  imports: [FormsModule],  
+  standalone: true, 
+  imports: [FormsModule, CommonModule], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Hello from BridgeLabz';  
+  title = 'Hello from BridgeLabz'; 
   logoUrl = 'assets/bridgelabz.png';
   url = 'https://www.bridgelabz.com';
-  userName = '';  
+  userName = ''; 
+  errorMessage = ''; 
 
   ngOnInit(): void {
     this.title = 'Hello from BridgeLabz.';
@@ -24,10 +26,13 @@ export class AppComponent {
   }
 
   updateTitle(): void {
-    if (this.userName) {
+    const namePattern = /^[A-Z][a-zA-Z]{2,}$/; 
+    if (this.userName && namePattern.test(this.userName)) {
       this.title = `Hello, ${this.userName} from BridgeLabz!`; 
+      this.errorMessage = ''; 
     } else {
       this.title = 'Hello from BridgeLabz';  
+      this.errorMessage = 'Please enter a valid name (at least 3 letters, starting with an uppercase letter).'; 
     }
   }
 }
